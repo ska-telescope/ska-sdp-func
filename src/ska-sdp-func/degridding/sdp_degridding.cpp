0b2a41ae269a6a3a33e647888329c4037079cc0e
Daniel Wright <daniel.wright@eng.ox.ac.uk>
2022-05-16 09:47:09 +0100

    HIP-236: Added finalised interface and associated Python bindings

--- src/ska-sdp-func/degridding/sdp_degridding.cpp
+++ src/ska-sdp-func/degridding/sdp_degridding.cpp
@@ -5,9 +5,17 @@
 #include "ska-sdp-func/utility/sdp_logging.h"
 
 void sdp_degridding(
-    const sdp_Mem* input,
-    sdp_Mem* output,
-    sdp_Error* status)
+        const sdp_Mem* grid,
+        const sdp_Mem* vis_coordinates,
+        const sdp_Mem* uv_kernel,
+        const sdp_Mem* w_kernel,
+        const int64_t uv_kernel_oversampling,
+        const int64_t w_kernel_oversampling,
+        const double theta,
+        const double wstep, 
+        const bool conjugate, 
+        sdp_Mem* vis,
+        sdp_Error* status)
 
 {
 
--- src/ska_sdp_func/degridding.py
+++ src/ska_sdp_func/degridding.py
@@ -5,105 +5,82 @@ from .utility import Error, Lib, Mem
 
 def degridding(
     grid,
-    u0,
-    v0,
-    w0,
+    vis_coordinates,
+    uv_kernel,
+    w_kernel,
+    uv_kernel_oversampling,
+    w_kernel_oversampling,
     theta,
     wstep,
-    uv_kernel,
-    uv_kernel_stride,
-    uv_kernel_oversampling,
-    w_kernel,
-    w_kernel_stride,
-    w_kernel_oversampling,
     conjugate,
-    vis_out,
+    vis,
 ):
     """
     Degridding visabilities.
 
     Degrids a previously gridded visabilty, based on the UV and W kernel input and returns the result.
 
-    :param grid: Input grid data
+    :param grid: 3D floating-point u,v,w coordinates of the visibilities
     :type grid: numpy.ndarray or cupy.ndarray
 
-    :param u0: Index of first coordinate on U axis
-    :type u0: int
-
-    :param v0: Index of first coordinate on V axis
-    :type v0: int
-
-    :param w0: Index of first coordinate on W axis
-    :type w0: int
-
-    :param theta: Conversion parameter from uv coordinates to xy coordinates (i.e. x=u*theta)
-    :type theta: int
-
-    :param wstep: Conversion parameter from w coordinates to z coordinates (i.e. z=w*wstep)
-    :type wstep: int
-
-    :param uv_kernel: U,V plane kernel
+    :param vis_coordinates: 3D floating-point u,v,w coordinates of the visibilities
+    :type vis_coordinates: numpy.ndarray or cupy.ndarray
+    
+    :param uv_kernel: u,v plane kernel
     :type uv_kernel: numpy.ndarray or cupy.ndarray
 
-    :param uv_kernel_stride: U,V plane kernel padding
-    :type uv_kernel_stride: int
-
-    :param uv_kernel_oversampling: U,V plane kernel oversampling
-    :type uv_kernel_oversampling: int
-
-    :param w_kernel: W plane Kernel
+    :param w_kernel: w plane Kernel
     :type w_kernel: numpy.ndarray or cupy.ndarray
 
-    :param w_kernel_stride: W plane Kernel padding
-    :type w_kernel_stride: int
+    :param uv_kernel_oversampling: u,v plane kernel oversampling
+    :type uv_kernel_oversampling: int
 
-    :param w_kernel_oversampling: W plane Kernel oversampling
+    :param w_kernel_oversampling: w plane Kernel oversampling
     :type w_kernel_oversampling: int
+
+    :param theta: Conversion parameter from uv coordinates to xy coordinates (i.e. x=u*theta)
+    :type theta: float
+
+    :param wstep: Conversion parameter from w coordinates to z coordinates (i.e. z=w*wstep)
+    :type wstep: float
 
     :param conjugate: Whether to generate conjugated visibilities
     :type conjugate: bool
 
-    :param vis_out: Output Visabilities
-    :type vis_out: numpy.ndarray or cupy.ndarray
+    :param vis: Output Visabilities
+    :type vis: numpy.ndarray or cupy.ndarray
     """
     mem_grid = Mem(grid)
+    mem_vis_coordinates = Mem(vis_coordinates)
     mem_uv_kernel = Mem(uv_kernel)
     mem_w_kernel = Mem(w_kernel)
-    mem_vis_out = Mem(vis_out)
+    mem_vis = Mem(vis)
     error_status = Error()
     lib_degridding = Lib.handle().sdp_degridding
     lib_degridding.argtypes = [
         Mem.handle_type(),
-        ctypes.c_int64,
-        ctypes.c_int64,
-        ctypes.c_int64,
-        ctypes.c_int64,
-        ctypes.c_int64,
+        Mem.handle_type(),
+        Mem.handle_type(),
         Mem.handle_type(),
         ctypes.c_int64,
         ctypes.c_int64,
-        Mem.handle_type(),
-        ctypes.c_int64,
-        ctypes.c_int64,
+        ctypes.c_double,
+        ctypes.c_double,
         ctypes.c_bool,
         Mem.handle_type(),
         Error.handle_type(),
     ]
     lib_degridding(
         mem_grid.handle(),
-        ctypes.c_int64(u0),
-        ctypes.c_int64(v0),
-        ctypes.c_int64(w0),
-        ctypes.c_int64(theta),
-        ctypes.c_int64(wstep),
+        mem_vis_coordinates.handle(),
         mem_uv_kernel.handle(),
-        ctypes.c_int64(uv_kernel),
-        ctypes.c_int64(uv_kernel_oversampling),
         mem_w_kernel.handle(),
-        ctypes.c_int64(w_kernel_stride),
+        ctypes.c_int64(uv_kernel_oversampling),
         ctypes.c_int64(w_kernel_oversampling),
-        ctypes.c_int64(conjugate),
-        mem_vis_out.handle(),
+        ctypes.c_double(theta),
+        ctypes.c_double(wstep),
+        ctypes.c_bool(conjugate),
+        mem_vis.handle(),
         error_status.handle(),
     )
     error_status.check()
