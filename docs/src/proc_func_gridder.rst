3b94f9016a625aa352f3eee96bfe920e7c9fe1c2
Anthony Griffin <anthony.griffin@aut.ac.nz>
2022-05-25 00:28:36 +1200

    Add initial documentation files

---
+++ docs/src/proc_func_gridder.rst
@@ -0,0 +1,20 @@
+.. _gridder_functions:
+
+*****************
+Gridder Functions
+*****************
+
+C/C++
+=====
+
+.. doxygengroup:: Gridder_func
+   :content-only:
+
+
+Python
+======
+
+.. autoclass:: ska_sdp_func.Gridder
+   :members:
+   :special-members: __init__
+   :exclude-members: Handle, handle, handle_type
--- src/ska-sdp-func/gridder/sdp_gridder.h
+++ src/ska-sdp-func/gridder/sdp_gridder.h
@@ -54,23 +54,23 @@ sdp_Gridder* sdp_gridder_create_plan(
  * @param status Error status.
  */
 void sdp_gridder_ms2dirty(
-		const sdp_Mem* uvw,
-		const sdp_Mem* freq_hz,
-		const sdp_Mem* vis,
-		const sdp_Mem* weight,
-		      sdp_Mem *dirty_image,
-		sdp_Gridder* plan,
-		sdp_Error* status
+        const sdp_Mem* uvw,
+        const sdp_Mem* freq_hz,
+        const sdp_Mem* vis,
+        const sdp_Mem* weight,
+              sdp_Mem *dirty_image,
+        sdp_Gridder* plan,
+        sdp_Error* status
     );
 
 void sdp_gridder_dirty2ms(
-		const sdp_Mem* uvw,
-		const sdp_Mem* freq_hz,
-			  sdp_Mem* vis,
-		const sdp_Mem* weight,
-			  sdp_Mem *dirty_image,   // even though this is an input, it is modified in place so can't be constant
-		sdp_Gridder* plan,
-		sdp_Error* status
+        const sdp_Mem* uvw,
+        const sdp_Mem* freq_hz,
+              sdp_Mem* vis,
+        const sdp_Mem* weight,
+              sdp_Mem *dirty_image,   // even though this is an input, it is modified in place so can't be constant
+        sdp_Gridder* plan,
+        sdp_Error* status
     );
 
 /**
