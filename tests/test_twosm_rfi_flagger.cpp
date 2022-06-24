3f793ce4c91bde7ccf9dd005fa19aa268a5cfd8a
Shahriar Etemadi Tajbakhsh <shahriar.etemaditajbakhsh@eng.ox.ac.uk>
2022-06-24 10:05:36 +0100

    HIP-261, a seg fault bug fixed

--- tests/test_twosm_rfi_flagger.cpp
+++ tests/test_twosm_rfi_flagger.cpp
@@ -28,13 +28,23 @@ static void check_results(
         return;
     }
     uint64_t wrong_flags = 0;
+    int counter0 = 0;
+    int counter1 = 0;
     for (uint64_t f = 0; f < num_elements; f++) {
+        if (flags[f] == 1){
+            counter0++;
+        }
+        if (predicted_flags[f] == 1){
+            counter1++;
+        }
         if (flags[f] != predicted_flags[f]){
             wrong_flags++;
             std::cout << "wrong pos: " << f <<std::endl;
         }
     }
     std::cout<< "Number of wrong flags: " << wrong_flags << std::endl;
+    std::cout<< "Number of flags: " << counter0 << std::endl;
+    std::cout<< "Number of predicted flags: " << counter1 << std::endl;
     assert(wrong_flags == 0);
     SDP_LOG_INFO("%s: Test passed", test_name);
 }
@@ -71,20 +81,18 @@ static void data_preparation(
         uint64_t time = 5 + (uint64_t) (((double) rand() / (double) RAND_MAX) * (num_timesamples - 5));
         // NOLINTNEXTLINE: rand() is not a problem for our use case.
         uint64_t freq = (uint64_t) (((double) rand() / (double) RAND_MAX) * (num_channels - 1));
-      //  std::cout << time << "    " << freq << std::endl;
+        //  std::cout << time << "    " << freq << std::endl;
         for (uint64_t b = 0; b < num_baselines; b++) {
-           for (uint64_t p = 0; p < num_pols; p++){
-               uint64_t pos = time * timesample_block + b * baseline_block +
-                              freq * frequency_block + p;
-               std::complex<input_type> ctemp(threshold + 0.01, 0);
-               //std::cout << pos << "   " << time << "   " << freq << std::endl;
-               visibilities[pos] = ctemp;
-               predicted_flags[pos] = 1;
-           }
+            for (uint64_t p = 0; p < num_pols; p++){
+                uint64_t pos = time * timesample_block + b * baseline_block +
+                               freq * frequency_block + p;
+                std::complex<input_type> ctemp(threshold + 0.01, 0);
+                //std::cout << pos << "   " << time << "   " << freq << std::endl;
+                visibilities[pos] = ctemp;
+                predicted_flags[pos] = 1;
+            }
         }
     }
-    std::cout << "vis: " << visibilities[104] << "     " << predicted_flags[104] <<std::endl;
-
 }
 
 
@@ -109,7 +117,7 @@ static void run_and_check(
     const uint64_t num_baselines       = 21;
     const uint64_t num_channels        = 200;
     const uint64_t num_pols            = 4;
-    int num_rfi_spikes = 20;
+    int num_rfi_spikes = 14;
 
 
     int64_t visibilities_shape[] = {
@@ -148,9 +156,9 @@ static void run_and_check(
                 (std::complex<float> *) sdp_mem_data(visibilities),
                 (int *) sdp_mem_data(predicted_flags),
                 (float *) sdp_mem_data(thresholds),
+                num_antennas,
                 num_timesamples,
                 num_channels,
-                num_baselines,
                 num_pols,
                 num_rfi_spikes
         );
@@ -178,6 +186,7 @@ static void run_and_check(
     sdp_Mem* antennas_in = sdp_mem_create_copy(antennas, antennas_location, status);
     sdp_Mem* flags_in = sdp_mem_create_copy(flags, flags_location, status);
     sdp_mem_set_read_only(flags_in, read_only_output);
+    const uint64_t num_elems = (uint64_t) sdp_mem_num_elements(visibilities);
     sdp_mem_ref_dec(visibilities);
     sdp_mem_ref_dec(thresholds);
     sdp_mem_ref_dec(flags);
@@ -187,6 +196,7 @@ static void run_and_check(
     sdp_twosm_algo_flagger(visibilities_in, thresholds_in, antennas_in, flags_in, status);
     sdp_mem_ref_dec(visibilities_in);
     sdp_mem_ref_dec(thresholds_in);
+
 
     // Copy the output for checking.
     sdp_Mem* flags_out = sdp_mem_create_copy(flags_in, SDP_MEM_CPU, status);
@@ -198,7 +208,7 @@ static void run_and_check(
                 test_name,
                 (int *) sdp_mem_data(flags_out),
                 (int *) sdp_mem_data(predicted_flags),
-                (uint64_t) sdp_mem_num_elements(visibilities),
+                num_elems,
                 status
         );
     }
