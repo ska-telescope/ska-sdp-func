aa5ca1ff320afdf541b1a2791b07733167ec2387
Shahriar Etemadi Tajbakhsh <shahriar.etemaditajbakhsh@eng.ox.ac.uk>
2022-07-21 19:50:01 +0100

    HIP-292, merge request comments addressed

--- src/ska-sdp-func/twosm_rfi_flagger/sdp_twosm_rfi_flagger.cpp
+++ src/ska-sdp-func/twosm_rfi_flagger/sdp_twosm_rfi_flagger.cpp
@@ -3,7 +3,7 @@
 #include <complex>
 #include "src/ska-sdp-func/twosm_rfi_flagger/sdp_twosm_rfi_flagger.h"#include "ska-sdp-func/utility/sdp_device_wrapper.h"
 #include "ska-sdp-func/utility/sdp_logging.h"
-#include <iostream>
+
 
 static void check_params(
         const sdp_Mem* vis,
@@ -78,12 +78,14 @@ static void twosm_rfi_flagger(
     double tol_margin_2sm_time = thresholds[0];
     double tol_margin_2sm_freq = thresholds[1];
 
+    // two-state machine algorithm applied to time dimension
     for (uint64_t a = 0; a < num_antennas; a++){
-        uint64_t b = antennas[a];
+        uint64_t b = antennas[a]; // selecting the id of an autocorrelation baseline (an "antenna to itself" type of
+        // baseline from the list of antennas
         for (uint64_t c = 0; c < num_channels; c++){
             for (uint64_t t = 1; t < num_timesamples; t++) {
-                uint64_t pos_current = t * timesample_block + b * baseline_block + c * channel_block;
-                uint64_t pos_minusone = (t - 1) * timesample_block + b * baseline_block + c * channel_block;
+                uint64_t pos_current = t * timesample_block + b * baseline_block + c * channel_block; // current position
+                uint64_t pos_minusone = (t - 1) * timesample_block + b * baseline_block + c * channel_block; //position at t-1
                 double vis0 = std::abs(visibilities[pos_current]);
                 double vis1 = std::abs(visibilities[pos_minusone]);
                 dv_between_cur_one = vis0 - vis1;
@@ -98,21 +100,24 @@ static void twosm_rfi_flagger(
                     bool cnd6 = flags[pos_minusone] == 1;
                     bool cnd7 = flags[pos_minusone] == 0;
 
+                    //detecting uphill transition ('good' to 'bad') or remaining in the bad position
                     if (cnd0 || (((cnd2 && cnd4) || (cnd3 && cnd5)) && cnd6)){
-                        for (uint64_t b = 0; b < num_baselines; b++){
+                        for (uint64_t base = 0; base < num_baselines; base++){
                             for (uint64_t p = 0; p < num_pols; p++){
-                                uint64_t pos = t * timesample_block + b * baseline_block + c * channel_block + p;
+                                uint64_t pos = t * timesample_block + base * baseline_block + c * channel_block + p;
                                 flags[pos] = 1;
                             }
                         }
                     }
+                    // downhill transition detection and back-tracking (if we detect a big downhill we know that we
+                    // have been in the 'bad' state, so we flag the previous samples)
                     if (cnd1 && (cnd7 == 0)){
                         uint64_t i = 0;
                         uint64_t pos = pos_minusone;
                         while (flags[pos] == 0 && t > i){
-                            for (uint64_t b = 0; b < num_baselines; b++){
+                            for (uint64_t base = 0; base < num_baselines; base++){
                                 for (uint64_t p = 0; p < num_pols; p++){
-                                    uint64_t pos = t * timesample_block + b * baseline_block + c * channel_block + p;
+                                    uint64_t pos = t * timesample_block + base * baseline_block + c * channel_block + p;
                                     flags[pos] = 1;
                                 }
                             }
@@ -125,6 +130,9 @@ static void twosm_rfi_flagger(
 
             }
         }
+        // two-state machine algorithm applied to frequency dimension
+        // we register the flag with number 2 in here to make calculating the union
+        // of the flags in frequency and time directions easier.
         for (uint64_t t = 0; t < num_timesamples; t++){
             for (uint64_t c = 1; c < num_channels; c++) {
                 uint64_t pos_current = t * timesample_block + b * baseline_block + c * channel_block;
@@ -142,21 +150,24 @@ static void twosm_rfi_flagger(
                     bool cnd5 = dv_ratio > 0;
                     bool cnd6 = flags[pos_minusone] == 2;
                     bool cnd7 = flags[pos_minusone] == 0 || flags[pos_minusone] == 1;
+                    //detecting uphill transition ('good' to 'bad') or remaining in the bad position
                     if (cnd0 || (((cnd2 && cnd4) || (cnd3 && cnd5)) && cnd6)){
-                        for (uint64_t b = 0; b < num_baselines; b++){
+                        for (uint64_t base = 0; base < num_baselines; base++){
                             for (uint64_t p = 0; p < num_pols; p++){
-                                uint64_t pos = t * timesample_block + b * baseline_block + c * channel_block + p;
+                                uint64_t pos = t * timesample_block + base * baseline_block + c * channel_block + p;
                                 flags[pos] = 2;
                             }
                         }
                     }
+                    // downhill transition detection and back-tracking (if we detect a big downhill we know that we
+                    // have been in the 'bad' state, so we flag the previous samples)
                     if (cnd1 && (cnd7 == 0)){
                         uint64_t i = 0;
                         uint64_t pos = pos_minusone;
                         while ((flags[pos] == 0 || flags[pos] == 1) && t > i){
-                            for (uint64_t b = 0; b < num_baselines; b++){
+                            for (uint64_t base = 0; base < num_baselines; base++){
                                 for (uint64_t p = 0; p < num_pols; p++){
-                                    uint64_t pos = t * timesample_block + b * baseline_block + c * channel_block + p;
+                                    uint64_t pos = t * timesample_block + base * baseline_block + c * channel_block + p;
                                     flags[pos] = 2;
                                 }
                             }
@@ -168,6 +179,7 @@ static void twosm_rfi_flagger(
             }
         }
     }
+    // calculating the union (replace all 2's and 1's with 1)
     for (int i = 0; i < num_elements; i++){
         if (flags[i] > 0){
             flags[i] = 1;
