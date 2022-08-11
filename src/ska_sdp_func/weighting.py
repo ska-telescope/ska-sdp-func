e1cea0b64f7235d11b608138d310c3d51bf1175f
Vlad Stolyarov <vlad.stolyarov@gmail.com>
2022-08-11 16:05:30 +0300

    HIP-314_flake8_fixed

--- src/ska_sdp_func/weighting.py
+++ src/ska_sdp_func/weighting.py
@@ -2,9 +2,12 @@
 
 """Module for the weighting functions."""
 
+import numpy as np
+
 
 def get_uv_range(uvw, freq_hz):
-    """Calculate uv-range in WL units given UVW-coordinates and frequency array."""
+    """Calculate uv-range in WL units given UVW-coordinates
+    and frequency array."""
     max_abs_uv = np.amax(np.abs(uvw[:, 0:1]))
     max_abs_uv *= freq_hz[-1] / 299792458.0
 
@@ -12,7 +15,8 @@ def get_uv_range(uvw, freq_hz):
 
 
 def uniform_weights(uvw, freq_hz, max_abs_uv, grid_size, weights):
-    """Calculate the number of hits per UV cell and use an invert as the weight"""
+    """Calculate the number of hits per UV cell and use an invert
+    as the weight"""
     grid_uv = np.zeros((grid_size + 1, grid_size + 1))
 
     for i in range(len(uvw)):
