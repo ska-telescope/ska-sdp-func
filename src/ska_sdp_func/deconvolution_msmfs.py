238e31e7a9041aaa6e3256531ecf053c01a1aaf6
Anthony Griffin <anthony.griffin@aut.ac.nz>
2022-08-25 23:35:07 +1200

    NZAPP-528 - Fix lint errors in deconvolution_msmfs.py

--- src/ska_sdp_func/deconvolution_msmfs.py
+++ src/ska_sdp_func/deconvolution_msmfs.py
@@ -12,20 +12,22 @@ from .utility import Error, Lib, Mem
 #
 # import numpy as np
 
+
 def perform_msmfs(
-        dirty_moment_images,
-        psf_moment_images,
-        dirty_moment_size,
-        num_scales,
-        num_taylor,  # 5
-        psf_moment_size,
-        image_border,
-        convolution_accuracy,
-        clean_loop_gain,
-        max_gaussian_sources_host,  # 10
-        scale_bias_factor,
-        clean_threshold):
-    """ Performs the entire MSMFS deconvolution.
+    dirty_moment_images,
+    psf_moment_images,
+    dirty_moment_size,
+    num_scales,
+    num_taylor,  # 5
+    psf_moment_size,
+    image_border,
+    convolution_accuracy,
+    clean_loop_gain,
+    max_gaussian_sources_host,  # 10
+    scale_bias_factor,
+    clean_threshold,
+):
+    """Performs the entire MSMFS deconvolution.
 
         dirty_moment_images and psf_moment_images assumed to be centred
         around origin with dirty_moment_images and have sufficient border
