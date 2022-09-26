2018501c34f2e8ab2783e8ba47fb979da5736505
Fred Dulwich <fred@mrao.cam.ac.uk>
2022-09-27 00:56:57 +0100

    HIP-326: Use a CMakeLists.txt file local to each directory, with relative paths.

---
+++ cmake/sdp_func_macros.cmake
@@ -0,0 +1,12 @@
+macro(SKA_SDP_FUNC_SRC)
+    # Get name of current directory.
+    get_filename_component(module ${CMAKE_CURRENT_LIST_DIR} NAME_WE)
+
+    # Iterate list of files supplied to the macro,
+    # generate a list called <directory name>_SRC,
+    # and add it to the parent scope.
+    foreach (file ${ARGN})
+        list(APPEND ${module}_SRC "src/ska-sdp-func/${module}/${file}")
+    endforeach()
+    set(${module}_SRC "${${module}_SRC}" PARENT_SCOPE)
+endmacro()
