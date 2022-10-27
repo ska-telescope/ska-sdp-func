a95f5c804ecdd63003a19c383241965d69714055
Fred Dulwich <fred@mrao.cam.ac.uk>
2022-10-27 14:55:44 +0100

    HIP-382: Use nonzero imaginary values in test.

--- tests/test_fft.py
+++ tests/test_fft.py
@@ -57,7 +57,9 @@ def test_fft_2d_ones():
 
 def test_fft_2d():
     """Test 2D FFT for consistency with numpy."""
-    input_data = numpy.random.random((256, 512)) + 0j
+    input_real = numpy.random.random((256, 512))
+    input_imag = numpy.random.random(input_real.shape)
+    input_data = input_real + 1j * input_imag
     output_ref = numpy.fft.fft2(input_data)
 
     # Test CPU version.
@@ -79,14 +81,24 @@ def test_fft_2d():
 def test_fft_2d_stack():
     """Test multiple stacked 2D FFTs for consistency with numpy."""
     num_slices = 4
-    input_data = numpy.random.random((num_slices, 256, 512)) + 0j
+    input_real = numpy.random.random((num_slices, 256, 512))
+    input_imag = numpy.random.random(input_real.shape)
+    input_data = input_real + 1j * input_imag
     output_ref = numpy.zeros_like(input_data)
     for i in range(num_slices):
         output_ref[i, :, :] = numpy.fft.fft2(input_data[i, :, :])
+
+    # Test CPU version.
+    output_cpu = numpy.zeros_like(input_data)
+    fft_cpu = Fft(input_data, output_cpu, 2, True)
+    fft_cpu.exec(input_data, output_cpu)
+    numpy.testing.assert_allclose(output_cpu, output_ref)
+
+    # Test GPU version.
     if cupy:
         input_gpu = cupy.asarray(input_data)
         output_gpu = cupy.zeros_like(input_gpu)
-        fft = Fft(input_gpu, output_gpu, 2, True)
-        fft.exec(input_gpu, output_gpu)
+        fft_gpu = Fft(input_gpu, output_gpu, 2, True)
+        fft_gpu.exec(input_gpu, output_gpu)
         output = cupy.asnumpy(output_gpu)
         numpy.testing.assert_allclose(output, output_ref)
