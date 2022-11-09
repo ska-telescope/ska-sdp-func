db463992ae8763179b979b52682fe51f380c401c
Vincent Morello <vmorello@gmail.com>
2022-11-09 18:33:06 +0000

    HIP-416: StructWrapper: base class for anything that wraps a C struct

--- src/ska_sdp_func/utility/__init__.py
+++ src/ska_sdp_func/utility/__init__.py
@@ -3,7 +3,8 @@
 """Import functions that we want to expose under ska_sdp_func.utility"""
 
 from .error import Error
+from .error_checking import CError
+from .struct_wrapper import StructWrapper
 from .lib import Lib
 from .mem import Mem
 from .sky_coord import SkyCoord
-from .error_checking import CError
