a523bba7f4c979a2a3176635b32dec2bc7fd7f2b
Vincent Morello <vmorello@gmail.com>
2022-11-10 12:42:19 +0000

    HIP-416: Remove Error class

--- src/ska_sdp_func/utility/__init__.py
+++ src/ska_sdp_func/utility/__init__.py
@@ -2,7 +2,6 @@
 
 """Import functions that we want to expose under ska_sdp_func.utility"""
 
-from .error import Error
 from .error_checking import CError
 from .struct_wrapper import StructWrapper
 from .lib import Lib
