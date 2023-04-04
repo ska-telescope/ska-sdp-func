27a63bc096e19a27ac960beab8db0574a6657d04
Daniel Wright <daniel.wright@eng.ox.ac.uk>
2023-04-04 16:34:14 +0100

    HIP-523: Tests for FFT based convolution

---
+++ tests/numeric_functions/test_fft_convolution.cpp
@@ -0,0 +1,114 @@
+/* See the LICENSE file at the top-level directory of this distribution. */
+
+#ifdef NDEBUG
+#undef NDEBUG
+#endif
+
+#include "ska-sdp-func/numeric_functions/sdp_fft_convolution.h"
+#include "ska-sdp-func/utility/sdp_logging.h"
+#include "ska-sdp-func/utility/sdp_mem.h"
+
+#include <cassert>
+
+
+static void run_and_check(
+    const char *test_name,
+    bool read_only_output,
+    sdp_MemType input_type,
+    sdp_MemType output_type,
+    sdp_MemLocation input_location,
+    sdp_MemLocation output_location,
+    sdp_Error *status)
+{
+
+    // settings
+    int64_t in1_dim = 1024;
+    int64_t in2_dim = 2048;
+
+    const int64_t in1_shape[] = {in1_dim, in1_dim};
+    const int64_t in2_shape[] = {in2_dim, in2_dim};
+
+    // create test data
+    sdp_Mem *in1 = sdp_mem_create(input_type, input_location, 2, in1_shape, status);
+    sdp_mem_random_fill(in1, status);
+
+    sdp_Mem *in2 = sdp_mem_create(input_type, input_location, 2, in2_shape, status);
+    sdp_mem_random_fill(in2, status);
+
+    // create output
+    sdp_Mem *out = sdp_mem_create(output_type, output_location, 2, in1_shape, status);
+    sdp_mem_clear_contents(out, status);
+    sdp_mem_set_read_only(out, read_only_output);
+
+    // call function to test
+    SDP_LOG_INFO("Running test: %s", test_name);
+    sdp_fft_convolution(
+        in1,
+        in2,
+        out,
+        status);
+
+    // release memory
+    sdp_mem_ref_dec(in1);
+    sdp_mem_ref_dec(in2);
+    sdp_mem_ref_dec(out);
+
+}
+
+int main()
+{
+    SDP_LOG_INFO("start of test:");
+
+    // happy paths
+    {
+        sdp_Error status = SDP_SUCCESS;
+        run_and_check("CPU, double precision", false,
+                      SDP_MEM_COMPLEX_DOUBLE, SDP_MEM_COMPLEX_DOUBLE,
+                      SDP_MEM_CPU, SDP_MEM_CPU, &status);
+        assert(status == SDP_SUCCESS);
+    }
+
+    {
+        sdp_Error status = SDP_SUCCESS;
+        run_and_check("CPU, single precision", false,
+                      SDP_MEM_COMPLEX_FLOAT, SDP_MEM_COMPLEX_FLOAT,
+                      SDP_MEM_CPU, SDP_MEM_CPU, &status);
+        assert(status == SDP_SUCCESS);
+    }
+
+    // unhappy paths
+    {
+        sdp_Error status = SDP_SUCCESS;
+        run_and_check("CPU, Type mis-match", false,
+                      SDP_MEM_COMPLEX_DOUBLE, SDP_MEM_COMPLEX_FLOAT,
+                      SDP_MEM_CPU, SDP_MEM_CPU, &status);
+        assert(status == SDP_ERR_DATA_TYPE);
+    }
+
+    {
+        sdp_Error status = SDP_SUCCESS;
+        run_and_check("CPU, read-only output", true,
+                      SDP_MEM_COMPLEX_DOUBLE, SDP_MEM_COMPLEX_DOUBLE,
+                      SDP_MEM_CPU, SDP_MEM_CPU, &status);
+        assert(status == SDP_ERR_RUNTIME);
+    }
+
+    {
+        sdp_Error status = SDP_SUCCESS;
+        run_and_check("CPU, memory location mis-match", false,
+                      SDP_MEM_COMPLEX_DOUBLE, SDP_MEM_COMPLEX_DOUBLE,
+                      SDP_MEM_CPU, SDP_MEM_GPU, &status);
+        assert(status == SDP_ERR_MEM_LOCATION);
+    }
+
+    {
+        sdp_Error status = SDP_SUCCESS;
+        run_and_check("CPU, non-complex input", false,
+                      SDP_MEM_DOUBLE, SDP_MEM_DOUBLE,
+                      SDP_MEM_CPU, SDP_MEM_CPU, &status);
+        assert(status == SDP_ERR_DATA_TYPE);
+    }
+
+    return 0;
+
+}
