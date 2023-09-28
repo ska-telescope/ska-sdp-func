60653219bf04f3120bb2902372318685c055ad34
Sebastiaan van der Tol <tol@astron.nl>
2023-09-28 13:28:39 +0200

    Add a generic sdp_element intended as wrapper for different dipole models

---
+++ src/ska-sdp-func/station_beam/sdp_element.cpp
@@ -0,0 +1,39 @@
+#include "sdp_element.h"
+
+#include <map>
+
+std::map<std::string,
+         std::tuple<make_function_t, destroy_function_t, evaluate_function_t>>&
+get_sdp_element_function_map() {
+  static std::map<std::string, std::tuple<make_function_t, destroy_function_t,
+                                          evaluate_function_t>>
+      function_map;
+  return function_map;
+}
+
+int register_sdp_element(const char* name,
+                              const make_function_t make_function,
+                              const destroy_function_t destroy_function,
+                              const evaluate_function_t evaluate_function) {
+  std::map<std::string, std::tuple<make_function_t, destroy_function_t,
+                                   evaluate_function_t>>& function_map =
+      get_sdp_element_function_map();
+  function_map[name] =
+      std::make_tuple(make_function, destroy_function, evaluate_function);
+  return 0;
+}
+
+sdp_Error get_sdp_element_functions(const char* name, make_function_t& make_function,
+                          destroy_function_t& destroy_function,
+                          evaluate_function_t& evaluate_function) {
+  std::map<std::string, std::tuple<make_function_t, destroy_function_t,
+                                   evaluate_function_t>>& function_map =
+      get_sdp_element_function_map();
+
+  if (!function_map.count(name)) return SDP_ERR_INVALID_ARGUMENT;
+
+  std::tie(make_function, destroy_function, evaluate_function) =
+      function_map[name];
+  return SDP_SUCCESS;
+  
+}
---
+++ src/ska-sdp-func/station_beam/sdp_element.h
@@ -0,0 +1,30 @@
+#include "../utility/sdp_errors.h"
+#include "../utility/sdp_mem.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef void* sdp_element_t;
+
+typedef sdp_element_t (*make_function_t)(const char* name);
+
+typedef void (*destroy_function_t)(sdp_element_t element_response);
+
+typedef void (*evaluate_function_t)(sdp_element_t element_response,
+                                    int num_points, const sdp_Mem* theta,
+                                    const sdp_Mem* phi, sdp_Mem* response);
+
+[[gnu::visibility("default")]] int register_sdp_element(
+    const char* name, const make_function_t make_function,
+    const destroy_function_t destroy_function,
+    const evaluate_function_t evaluate_function);
+
+[[gnu::visibility("default")]] sdp_Error get_sdp_element_functions(
+    const char* name, make_function_t& make_function,
+    destroy_function_t& destroy_function,
+    evaluate_function_t& evaluate_function);
+
+#ifdef __cplusplus
+}
+#endif
