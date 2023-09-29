fffd52845d8ff11970560f76ea1dd5b8aec7a17f
Sebastiaan van der Tol <tol@astron.nl>
2023-09-29 18:30:18 +0200

    Uncrustify

--- src/ska-sdp-func/station_beam/sdp_element.cpp
+++ src/ska-sdp-func/station_beam/sdp_element.cpp
@@ -4,39 +4,48 @@
 #include <string>
 
 namespace {
-  std::map<std::string,
-          std::tuple<make_function_t, destroy_function_t, evaluate_function_t>>&
-  get_sdp_element_function_map() {
+std::map<std::string,
+        std::tuple<make_function_t, destroy_function_t,
+        evaluate_function_t> >&get_sdp_element_function_map()
+{
     static std::map<std::string, std::tuple<make_function_t, destroy_function_t,
-                                            evaluate_function_t>>
-        function_map;
+            evaluate_function_t> >
+    function_map;
     return function_map;
-  }
+}
 }
 
-int sdp_register_element(const char* name,
-                              const make_function_t make_function,
-                              const destroy_function_t destroy_function,
-                              const evaluate_function_t evaluate_function) {
-  std::map<std::string, std::tuple<make_function_t, destroy_function_t,
-                                   evaluate_function_t>>& function_map =
-      get_sdp_element_function_map();
-  function_map[name] =
-      std::make_tuple(make_function, destroy_function, evaluate_function);
-  return 0;
+
+int sdp_register_element(
+        const char* name,
+        const make_function_t make_function,
+        const destroy_function_t destroy_function,
+        const evaluate_function_t evaluate_function
+)
+{
+    std::map<std::string, std::tuple<make_function_t, destroy_function_t,
+            evaluate_function_t> >& function_map =
+            get_sdp_element_function_map();
+    function_map[name] =
+            std::make_tuple(make_function, destroy_function, evaluate_function);
+    return 0;
 }
 
-sdp_Error sdp_get_element_functions(const char* name, make_function_t& make_function,
-                          destroy_function_t& destroy_function,
-                          evaluate_function_t& evaluate_function) {
-  std::map<std::string, std::tuple<make_function_t, destroy_function_t,
-                                   evaluate_function_t>>& function_map =
-      get_sdp_element_function_map();
 
-  if (!function_map.count(name)) return SDP_ERR_INVALID_ARGUMENT;
+sdp_Error sdp_get_element_functions(
+        const char* name,
+        make_function_t& make_function,
+        destroy_function_t& destroy_function,
+        evaluate_function_t& evaluate_function
+)
+{
+    std::map<std::string, std::tuple<make_function_t, destroy_function_t,
+            evaluate_function_t> >& function_map =
+            get_sdp_element_function_map();
 
-  std::tie(make_function, destroy_function, evaluate_function) =
-      function_map[name];
-  return SDP_SUCCESS;
-  
+    if (!function_map.count(name)) return SDP_ERR_INVALID_ARGUMENT;
+
+    std::tie(make_function, destroy_function, evaluate_function) =
+            function_map[name];
+    return SDP_SUCCESS;
 }
--- src/ska-sdp-func/station_beam/sdp_element.h
+++ src/ska-sdp-func/station_beam/sdp_element.h
@@ -11,19 +11,27 @@ typedef sdp_element_t (*make_function_t)(const char* name);
 
 typedef void (*destroy_function_t)(sdp_element_t element_response);
 
-typedef void (*evaluate_function_t)(sdp_element_t element_response,
-                                    int num_points, const sdp_Mem* theta,
-                                    const sdp_Mem* phi, sdp_Mem* response);
+typedef void (*evaluate_function_t)(
+        sdp_element_t element_response,
+        int num_points,
+        const sdp_Mem* theta,
+        const sdp_Mem* phi,
+        sdp_Mem* response
+);
 
 [[gnu::visibility("default")]] int sdp_register_element(
-    const char* name, const make_function_t make_function,
-    const destroy_function_t destroy_function,
-    const evaluate_function_t evaluate_function);
+        const char* name,
+        const make_function_t make_function,
+        const destroy_function_t destroy_function,
+        const evaluate_function_t evaluate_function
+);
 
 [[gnu::visibility("default")]] sdp_Error sdp_get_element_functions(
-    const char* name, make_function_t& make_function,
-    destroy_function_t& destroy_function,
-    evaluate_function_t& evaluate_function);
+        const char* name,
+        make_function_t& make_function,
+        destroy_function_t& destroy_function,
+        evaluate_function_t& evaluate_function
+);
 
 #ifdef __cplusplus
 }
