c839a33a3f35ac85b83b05ea84593516f3afe04f
Fred Dulwich <fred@mrao.cam.ac.uk>
2024-02-21 00:00:41 +0000

    HIP-820: Black formatting.

--- tests/grid_data/test_sub_grid.py
+++ tests/grid_data/test_sub_grid.py
@@ -41,11 +41,13 @@ def generate_uvw(ant_config, num_times, length_hours, longitude_rad):
         ha0 = gst - ra0
 
         # Rotation matrix to convert from (x,y,z) to (u,v,w).
-        mat = numpy.array([
-            [sin(ha0), cos(ha0), 0],
-            [-sin(dec0) * cos(ha0), sin(dec0) * sin(ha0), cos(dec0)],
-            [cos(dec0) * cos(ha0), -cos(dec0) * sin(ha0), sin(dec0)]
-        ])
+        mat = numpy.array(
+            [
+                [sin(ha0), cos(ha0), 0],
+                [-sin(dec0) * cos(ha0), sin(dec0) * sin(ha0), cos(dec0)],
+                [cos(dec0) * cos(ha0), -cos(dec0) * sin(ha0), sin(dec0)],
+            ]
+        )
         (u, v, w) = numpy.dot(mat, numpy.array((ant_x, ant_y, ant_z)))
 
         # Generate baseline pairs.
@@ -63,35 +65,37 @@ def test_sub_grid():
     """Test gridding on a sub-grid."""
 
     # VLA D-configuration.
-    ant_config = numpy.array([
-        [-1601188.989351, -5042000.518599, 3554843.384480],
-        [-1601225.230987, -5041980.390730, 3554855.657987],
-        [-1601265.110332, -5041982.563379, 3554834.816409],
-        [-1601315.874282, -5041985.324465, 3554808.263784],
-        [-1601376.950042, -5041988.682890, 3554776.344871],
-        [-1601447.176774, -5041992.529191, 3554739.647266],
-        [-1601526.335275, -5041996.876364, 3554698.284889],
-        [-1601614.061201, -5042001.676547, 3554652.455603],
-        [-1601709.987416, -5042006.942534, 3554602.306306],
-        [-1601192.424192, -5042022.883542, 3554810.383317],
-        [-1601150.027460, -5042000.630731, 3554860.703495],
-        [-1601114.318178, -5042023.187696, 3554844.922416],
-        [-1601068.771188, -5042051.929370, 3554824.767363],
-        [-1601014.405657, -5042086.261585, 3554800.768970],
-        [-1600951.545716, -5042125.927280, 3554772.987195],
-        [-1600880.545264, -5042170.376845, 3554741.425036],
-        [-1600801.880602, -5042219.386677, 3554706.382285],
-        [-1600715.918854, -5042273.142150, 3554668.128757],
-        [-1601185.553970, -5041978.191573, 3554876.382645],
-        [-1601180.820941, -5041947.459898, 3554921.573373],
-        [-1601177.368455, -5041925.069104, 3554954.532566],
-        [-1601173.903632, -5041902.679083, 3554987.485762],
-        [-1601168.735762, -5041869.062707, 3555036.885577],
-        [-1601162.553007, -5041829.021602, 3555095.854771],
-        [-1601155.593706, -5041783.860938, 3555162.327771],
-        [-1601147.885235, -5041733.855114, 3555235.914849],
-        [-1601139.483292, -5041679.021042, 3555316.478099],
-    ])
+    ant_config = numpy.array(
+        [
+            [-1601188.989351, -5042000.518599, 3554843.384480],
+            [-1601225.230987, -5041980.390730, 3554855.657987],
+            [-1601265.110332, -5041982.563379, 3554834.816409],
+            [-1601315.874282, -5041985.324465, 3554808.263784],
+            [-1601376.950042, -5041988.682890, 3554776.344871],
+            [-1601447.176774, -5041992.529191, 3554739.647266],
+            [-1601526.335275, -5041996.876364, 3554698.284889],
+            [-1601614.061201, -5042001.676547, 3554652.455603],
+            [-1601709.987416, -5042006.942534, 3554602.306306],
+            [-1601192.424192, -5042022.883542, 3554810.383317],
+            [-1601150.027460, -5042000.630731, 3554860.703495],
+            [-1601114.318178, -5042023.187696, 3554844.922416],
+            [-1601068.771188, -5042051.929370, 3554824.767363],
+            [-1601014.405657, -5042086.261585, 3554800.768970],
+            [-1600951.545716, -5042125.927280, 3554772.987195],
+            [-1600880.545264, -5042170.376845, 3554741.425036],
+            [-1600801.880602, -5042219.386677, 3554706.382285],
+            [-1600715.918854, -5042273.142150, 3554668.128757],
+            [-1601185.553970, -5041978.191573, 3554876.382645],
+            [-1601180.820941, -5041947.459898, 3554921.573373],
+            [-1601177.368455, -5041925.069104, 3554954.532566],
+            [-1601173.903632, -5041902.679083, 3554987.485762],
+            [-1601168.735762, -5041869.062707, 3555036.885577],
+            [-1601162.553007, -5041829.021602, 3555095.854771],
+            [-1601155.593706, -5041783.860938, 3555162.327771],
+            [-1601147.885235, -5041733.855114, 3555235.914849],
+            [-1601139.483292, -5041679.021042, 3555316.478099],
+        ]
+    )
 
     # Generate (u,v,w) coordinates.
     longitude_rad = -107.566 * pi / 180.0
@@ -110,8 +114,9 @@ def test_sub_grid():
     # plt.show()
 
     # Define other test parameters.
-    vis = numpy.random.randn(num_times * num_baselines, num_channels) + \
-        1j * numpy.random.randn(num_times * num_baselines, num_channels)
+    vis = numpy.random.randn(
+        num_times * num_baselines, num_channels
+    ) + 1j * numpy.random.randn(num_times * num_baselines, num_channels)
     vis[:, :] = 1.0
     weight = numpy.ones(vis.shape)
     image_size = 2000
@@ -134,9 +139,20 @@ def test_sub_grid():
         full_grid = cupy.zeros(
             [full_grid_size, full_grid_size], dtype=cupy.complex128
         )
-        grid_uvw_es(uvw_gpu, vis_gpu, weight_gpu, freq_gpu,
-                    image_size, epsilon, cell_size_rad, w_scale, min_plane_w,
-                    0, 0, sub_grid_w, full_grid
+        grid_uvw_es(
+            uvw_gpu,
+            vis_gpu,
+            weight_gpu,
+            freq_gpu,
+            image_size,
+            epsilon,
+            cell_size_rad,
+            w_scale,
+            min_plane_w,
+            0,
+            0,
+            sub_grid_w,
+            full_grid,
         )
 
         # Do gridding onto a sub-grid.
@@ -146,9 +162,20 @@ def test_sub_grid():
         sub_grid = cupy.zeros(
             [sub_grid_size, sub_grid_size], dtype=cupy.complex128
         )
-        grid_uvw_es(uvw_gpu, vis_gpu, weight_gpu, freq_gpu,
-                    image_size, epsilon, cell_size_rad, w_scale, min_plane_w,
-                    sub_grid_start_u, sub_grid_start_v, sub_grid_w, sub_grid
+        grid_uvw_es(
+            uvw_gpu,
+            vis_gpu,
+            weight_gpu,
+            freq_gpu,
+            image_size,
+            epsilon,
+            cell_size_rad,
+            w_scale,
+            min_plane_w,
+            sub_grid_start_u,
+            sub_grid_start_v,
+            sub_grid_w,
+            sub_grid,
         )
 
         # Check results match.
@@ -157,9 +184,9 @@ def test_sub_grid():
         numpy.testing.assert_array_almost_equal(
             sub_grid_cpu,
             full_grid_cpu[
-                sub_grid_start_u:sub_grid_start_u + sub_grid_size,
-                sub_grid_start_v:sub_grid_start_v + sub_grid_size,
-            ]
+                sub_grid_start_u : sub_grid_start_u + sub_grid_size,
+                sub_grid_start_v : sub_grid_start_v + sub_grid_size,
+            ],
         )
 
         # Plot results to check.
