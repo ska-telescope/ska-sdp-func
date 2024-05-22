1a6408647389dd2cc97a5c1ad763a0e9606ad327
Fred Dulwich <fred@mrao.cam.ac.uk>
2024-05-22 09:06:56 +0100

    HIP-927: Add test for inverse FFT.

--- tests/fourier_transforms/test_fft.py
+++ tests/fourier_transforms/test_fft.py
@@ -78,6 +78,31 @@ def test_fft_2d():
         numpy.testing.assert_allclose(output, output_ref)
 
 
+def test_fft_2d_inverse():
+    """Test 2D iFFT for consistency with numpy."""
+    input_real = numpy.random.random((256, 512))
+    input_imag = numpy.random.random(input_real.shape)
+    input_data = input_real + 1j * input_imag
+    output_ref = numpy.fft.ifft2(input_data)
+
+    # Test CPU version.
+    output_cpu = numpy.zeros_like(input_data)
+    fft_cpu = Fft(input_data, output_cpu, 2, False)
+    fft_cpu.exec(input_data, output_cpu)
+    output_cpu /= input_data.size
+    numpy.testing.assert_allclose(output_cpu, output_ref)
+
+    # Test GPU version.
+    if cupy:
+        input_gpu = cupy.asarray(input_data)
+        output_gpu = cupy.zeros_like(input_gpu)
+        fft_gpu = Fft(input_gpu, output_gpu, 2, False)
+        fft_gpu.exec(input_gpu, output_gpu)
+        output = cupy.asnumpy(output_gpu)
+        output /= input_data.size
+        numpy.testing.assert_allclose(output, output_ref)
+
+
 def test_fft_2d_stack():
     """Test multiple stacked 2D FFTs for consistency with numpy."""
     num_slices = 4
