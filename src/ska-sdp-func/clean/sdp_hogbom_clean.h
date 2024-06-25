609c1197b366c34a0347ea9ac7efb2d23e9845e0
Daniel Wright <daniel.wright@eng.ox.ac.uk>
2024-06-25 18:13:08 +0100

    hip-942: Adding Hogbom clean

---
+++ src/ska-sdp-func/clean/sdp_hogbom_clean.h
@@ -0,0 +1,47 @@
+/* See the LICENSE file at the top-level directory of this distribution. */
+
+#ifndef SKA_SDP_PROC_FUNC_HOGBOM_CLEAN_H_
+#define SKA_SDP_PROC_FUNC_HOGBOM_CLEAN_H_
+
+/**
+ * @file sdp_hogbom_clean.h
+ */
+
+#include "ska-sdp-func/utility/sdp_mem.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Perform the Hogbom CLEAN algorithm on a dirty image
+ *
+ * @param dirty_img Input dirty image.
+ * @param psf Input Point Spread Function.
+ * @param cbeam_details Input shape of CLEAN beam, with the size of the array to be generated [BMAJ, BMINN, THETA, SIZE]
+ * @param loop_gain Gain to be used in the CLEAN loop (typically 0.1)
+ * @param threshold Minimum intensity of peak to search for, loop terminates if peak is found under this threshold.
+ * @param cycle_limit Maximum nuber of loops to perform, if the stop threshold is not reached first.
+ * @param clean_model Map of CLEAN components, unconvolved pixels.
+ * @param residual Residual image, flux remaining after CLEANing.
+ * @param skymodel Output Skymodel, CLEAN components convolved with CLEAN beam + residuals.
+ * @param status Error status.
+ */
+void sdp_hogbom_clean(
+        const sdp_Mem* dirty_img,
+        const sdp_Mem* psf,
+        const sdp_Mem* cbeam_details,
+        const double loop_gain,
+        const double threshold,
+        const int cycle_limit,
+        sdp_Mem* clean_model,
+        sdp_Mem* residual,
+        sdp_Mem* skymodel,
+        sdp_Error* status
+);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* include guard */
--- src/ska_sdp_func/numeric_functions/fft_convolution.py
+++ src/ska_sdp_func/numeric_functions/fft_convolution.py
@@ -5,22 +5,15 @@ from ..utility import Lib, Mem
 Lib.wrap_func(
     "sdp_fft_convolution",
     restype=None,
-    argtypes=[
-        Mem.handle_type(),
-        Mem.handle_type(),
-        Mem.handle_type()
-    ],
+    argtypes=[Mem.handle_type(), Mem.handle_type(), Mem.handle_type()],
     check_errcode=True,
 )
 
 
-def fft_convolution(
-    in1,
-    in2,
-    out
-):
-    """Implimentation of the convolution theorem. Assumes that inputs are square.
-    returned convolution is the same size as in1, similar to scipy.signal.convolve "same" mode.
+def fft_convolution(in1, in2, out):
+    """Implimentation of the convolution theorem. Assumes that inputs
+    are square. returned convolution is the same size as in1,
+    similar to scipy.signal.convolve "same" mode.
 
     Parameters can be either numpy or cupy arrays, but must all be consistent.
     Computation is performed either on the CPU or GPU as appropriate.
