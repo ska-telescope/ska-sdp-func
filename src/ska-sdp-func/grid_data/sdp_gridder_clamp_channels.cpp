31cb82b228f005a147b5e16ea0951b5563d84338
Fred Dulwich <fred@mrao.cam.ac.uk>
2024-07-24 10:24:51 +0100

    HIP-984: Add GPU kernel for clamp_channels.

--- src/ska-sdp-func/grid_data/sdp_gridder_clamp_channels.cpp
+++ src/ska-sdp-func/grid_data/sdp_gridder_clamp_channels.cpp
@@ -3,8 +3,11 @@
 #include "ska-sdp-func/grid_data/sdp_gridder_clamp_channels.h"
 #include "ska-sdp-func/utility/sdp_mem_view.h"
 
+// Begin anonymous namespace for file-local functions.
+namespace {
 
-void sdp_gridder_clamp_channels_single(
+template<typename UVW_TYPE>
+void clamp_channels_single(
         const sdp_Mem* uvws,
         const int dim,
         const double freq0_hz,
@@ -16,9 +19,10 @@ void sdp_gridder_clamp_channels_single(
         sdp_Error* status
 )
 {
+    if (*status) return;
     const double eta = 1e-2;
     const int range_includes_zero = (min_u <= 0 && max_u > 0);
-    sdp_MemViewCpu<const double, 2> uvws_;
+    sdp_MemViewCpu<const UVW_TYPE, 2> uvws_;
     sdp_MemViewCpu<int, 1> start_chs_, end_chs_;
     sdp_mem_check_and_view(uvws, &uvws_, status);
     sdp_mem_check_and_view(start_ch, &start_chs_, status);
@@ -45,5 +49,87 @@ void sdp_gridder_clamp_channels_single(
             end_chs_(i) = 0;
         }
         end_chs_(i) = MAX(end_chs_(i), start_chs_(i));
+    }
+}
+
+} // End anonymous namespace for file-local functions.
+
+
+void sdp_gridder_clamp_channels_single(
+        const sdp_Mem* uvws,
+        const int dim,
+        const double freq0_hz,
+        const double dfreq_hz,
+        sdp_Mem* start_ch,
+        sdp_Mem* end_ch,
+        const double min_u,
+        const double max_u,
+        sdp_Error* status
+)
+{
+    if (*status) return;
+    const sdp_MemLocation loc = sdp_mem_location(uvws);
+    if (loc == SDP_MEM_CPU)
+    {
+        if (sdp_mem_type(uvws) == SDP_MEM_DOUBLE)
+        {
+            clamp_channels_single<double>(uvws, dim, freq0_hz, dfreq_hz,
+                    start_ch, end_ch, min_u, max_u, status
+            );
+        }
+        else if (sdp_mem_type(uvws) == SDP_MEM_FLOAT)
+        {
+            clamp_channels_single<float>(uvws, dim, freq0_hz, dfreq_hz,
+                    start_ch, end_ch, min_u, max_u, status
+            );
+        }
+        else
+        {
+            *status = SDP_ERR_DATA_TYPE;
+            return;
+        }
+    }
+    else if (loc == SDP_MEM_GPU)
+    {
+        // Call the kernel.
+        uint64_t num_threads[] = {256, 1, 1}, num_blocks[] = {1, 1, 1};
+        const int64_t num_elements = sdp_mem_shape_dim(uvws, 0);
+        num_blocks[0] = (num_elements + num_threads[0] - 1) / num_threads[0];
+        sdp_MemViewGpu<const double, 2> uvws_dbl;
+        sdp_MemViewGpu<const float, 2> uvws_flt;
+        sdp_MemViewGpu<int, 1> start_chs_, end_chs_;
+        sdp_mem_check_and_view(start_ch, &start_chs_, status);
+        sdp_mem_check_and_view(end_ch, &end_chs_, status);
+        const char* kernel_name = 0;
+        int is_dbl = 0;
+        if (sdp_mem_type(uvws) == SDP_MEM_DOUBLE)
+        {
+            is_dbl = 1;
+            sdp_mem_check_and_view(uvws, &uvws_dbl, status);
+            kernel_name = "sdp_gridder_clamp_channels_single<double>";
+        }
+        else if (sdp_mem_type(uvws) == SDP_MEM_FLOAT)
+        {
+            is_dbl = 0;
+            sdp_mem_check_and_view(uvws, &uvws_flt, status);
+            kernel_name = "sdp_gridder_clamp_channels_single<float>";
+        }
+        else
+        {
+            *status = SDP_ERR_DATA_TYPE;
+        }
+        const void* arg[] = {
+            is_dbl ? (const void*)&uvws_dbl : (const void*)&uvws_flt,
+            (const void*)&dim,
+            (const void*)&freq0_hz,
+            (const void*)&dfreq_hz,
+            (const void*)&start_chs_,
+            (const void*)&end_chs_,
+            (const void*)&min_u,
+            (const void*)&max_u,
+        };
+        sdp_launch_cuda_kernel(kernel_name,
+                num_blocks, num_threads, 0, 0, arg, status
+        );
     }
 }
