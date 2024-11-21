d09cf4e1ad49aa42542c2f2955a141971079a208
Saeid Aliei <Saeid.Aliei@skao.int>
2024-11-21 15:59:54 +0000

    HIP-1113 fix python grid_data init file to include degridder

--- simple/src/ska_sdp_func/grid_data/__init__.py
+++ simple/src/ska_sdp_func/grid_data/__init__.py
@@ -3,6 +3,7 @@
 """Import functions that we want to expose under ska_sdp_func.grid_data"""
 
 from .grid_wstack_wtower import (
+    wstack_wtower_degrid_all,
     wstack_wtower_grid_all,
 )
 from .gridder_utils import (
