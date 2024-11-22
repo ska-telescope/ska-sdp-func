2157a9e29b787792aed7a394112b4d723b9e583d
Saeid Aliei <saeid.aliei@skao.int>
2024-11-22 19:36:05 +0000

    HIP-1113 remove _mm256_reduce_add_ps function

--- simple/src/ska-sdp-func/grid_data/sdp_gridder_wtower_uvw.cpp
+++ simple/src/ska-sdp-func/grid_data/sdp_gridder_wtower_uvw.cpp
@@ -44,7 +44,8 @@ namespace {
 
 #ifdef AVX2
 // There is no direct equivalent AVX2 instruction to _mm512_reduce_pd, 
-// so we need to define one for ourself.
+// so we need to define one for ourself. Note that this method won't 
+// work for the single precision.
 inline double _mm256_reduce_add_pd(__m256d vec) {
 	// Shuffle the high and low halves of the vector
 	// and move high 128-bit to low
@@ -60,23 +61,6 @@ inline double _mm256_reduce_add_pd(__m256d vec) {
 
 	// Extract the scalar sum, each element is the sum
 	return _mm_cvtsd_f64(_mm256_castpd256_pd128(sum));
-}
-
-inline float _mm256_reduce_add_ps(__m256d vec) {
-	// Shuffle the high and low halves of the vector
-	// and move high 128-bit to low
-	__m256d shuf = _mm256_permute2f128_ps(vec, vec, 1);
-	// Add high and low halves
-	__m256d sum = _mm256_add_ps(vec, shuf);
-
-	// Shuffle within the low 128 bits,
-	// and swap the adjacent pairs
-	shuf = _mm256_permute_ps(sum, 0b0101);
-	// Add adjacent pairs
-	sum = _mm256_add_ps(sum, shuf);
-
-	// Extract the scalar sum, each element is the sum
-	return _mm_cvtsd_f64(_mm256_castpd256_ps128(sum));
 }
 #endif // AVX2
 
