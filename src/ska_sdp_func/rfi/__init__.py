4876e4fe86dafa4e22b5e903d8df41f95d33f576
shahriar_etemadi_tajbakhsh <shahriar.etemaditajbakhsh@eng.ox.ac.uk>
2024-12-19 15:27:46 +0000

    HIP-1142, old rfi code removed

--- src/ska_sdp_func/rfi/__init__.py
+++
@@ -1,6 +0,0 @@
-# See the LICENSE file at the top-level directory of this distribution.
-
-"""Import functions that we want to expose under ska_sdp_func.rfi"""
-
-from .rfi_flagger import sum_threshold_rfi_flagger
-from .twosm_rfi_flagger import twosm_rfi_flagger
